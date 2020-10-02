96686562349d1e8b0effc9235dffeea32f19ba0a
Danilo Patanè <d.patane98@gmail.com>
2020-10-02 16:19:19 +0200

    Problem with get

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-  $titolo  = $_GET["title"];
+  $bruttaParola  = $_GET["text"];
   $paragrafo = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo bruttaparola. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-  $badWordCensored = str_replace("bruttaparola", "***", $paragrafo);
+  $badWordCensored = str_replace($bruttaParola, "***", $paragrafo);
 
  ?>
 
@@ -13,7 +13,6 @@
     <title>PHP BADWORDS</title>
   </head>
   <body>
-    <h1><?php echo $titolo; ?></h1>
     <p><?php echo $badWordCensored; ?></p>
   </body>
 </html>
